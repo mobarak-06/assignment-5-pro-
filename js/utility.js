3d6dc75add9a7c5df761634f5b608a5ae176a585
Mobarak Hossain <mobarakhossain121838@gmail.com>
2024-04-17 10:08:10 +0600

    1st ticket section added

---
+++ js/utility.js
@@ -0,0 +1,9 @@
+function hideBackground(){
+    const main = document.getElementById('main');
+    main.classList.add('hidden')
+}
+
+function showBackground(){
+    const main2 = document.getElementById('main2');
+    main2.classList.remove('hidden');
+}
