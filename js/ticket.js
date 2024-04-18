53d7a1ba520b6483425729dbfabc7696d26d3433
Mobarak Hossain <mobarakhossain121838@gmail.com>
2024-04-18 13:12:50 +0600

    1st js added

--- js/ticket.js
+++ js/ticket.js
@@ -1,4 +1,57 @@
-function handleClick(){
-    showBackground()
-    hideBackground()
+function handleClick(event){
+   const seat = event.innerText;
+
+   const price = document.getElementById('price').innerText;
+    const convertedPrice = parseInt(price);
+
+   const busClass = document.getElementById('bus-class').innerText;
+
+
+       const busDetails = document.getElementById('economoy');
+       
+       const div = document.createElement('div');
+       const p = document.createElement('p');
+       const p2 = document.createElement('p');
+       const p3 = document.createElement('p');
+
+       p.innerText = seat;
+       p2.innerText = busClass;
+       p3.innerText = convertedPrice;
+
+       div.appendChild(p);
+       div.appendChild(p2);
+       div.appendChild(p3);
+
+       busDetails.appendChild(div);
+
+       div.classList.add('seat-count');
+
+       event.setAttribute('disabled',false);
+       
+ 
+       const firstCart = getConvertedValue('current-seat');
+       if (firstCart + 1 > 4) {
+           alert('seat limit finish ');
+           return;
+       }
+
+       event.style.backgroundColor = '#1DD100'
+         
+       
+
+       const currentSeat = getConvertedValue('current-seat');
+       const updatedSeat = currentSeat +1;
+       document.getElementById('current-seat').innerText = updatedSeat;
+
+       const totalPrice = getConvertedValue('total-price');
+       const updatedTotalPrice = totalPrice + convertedPrice;
+       document.getElementById('total-price').innerText = updatedTotalPrice;
+
+       const grandTotal = getConvertedValue('grand-total');
+       const updatedGrandTotal = totalPrice + convertedPrice;
+       document.getElementById('grand-total').innerText = updatedTotalPrice;
+
+      
+      
 }
+ 
--- js/utility.js
+++ js/utility.js
@@ -7,3 +7,9 @@ function showBackground(){
     const main2 = document.getElementById('main2');
     main2.classList.remove('hidden');
 }
+
+function getConvertedValue(id){
+    const element = document.getElementById(id).innerText;
+    const convertedElement = parseInt(element);
+    return convertedElement;
+}
